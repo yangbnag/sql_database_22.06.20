ae2d90aace4d51717adc3f58b73be80b47d9fe80
yanbnag <yanbam@naver.com>
2022-07-17 21:00:46 +0900

    22.07.17

--- sql예제/spring4.sql
+++ sql예제/spring4.sql
@@ -1,26 +1,8 @@
-
-select * from tbl_score;
-
-
-
-CREATE SEQUENCE seq_board;
-
-DROP TABLE board;
-CREATE TABLE board (
-    board_no NUMBER(10),
-    writer VARCHAR2(20) NOT NULL,
-    title VARCHAR2(200) NOT NULL,
-    content VARCHAR2(2000),
-    view_cnt NUMBER(10) DEFAULT 0,
-    reg_date DATE DEFAULT SYSDATE,
-    CONSTRAINT pk_board PRIMARY KEY (board_no)
-);
-
-INSERT INTO board 
-(board_no, writer, title, content)
-VALUES (seq_board.nextval, '²Ü²ÜÀÌ','¾ÆÄ§¹ä¸ÔÀÚ' ,'¾ÆÄ§¿¡´Â µÎ³¢- ºí¶óºí¶ó');
-
-commit;
-
 SELECT * FROM board;
+
+UPDATE board SET view_cnt = view_cnt+1 where board_no = 1;
+
+ UPDATE board SET view_cnt = view_cnt+1 where board_no = 11 ;
+ 
+ commit;
 
